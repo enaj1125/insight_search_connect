048906eb12427c6f34d3fbdefae306dc9fac1aa7
Yan Jiang <enaj1125@gmail.com>
2017-02-17 00:04:52 -0800

    Update enable.sh

--- elasticsearch_files/enable.sh
+++ elasticsearch_files/enable.sh
@@ -1,3 +1,5 @@
+# this query search for a certain type of file
+
 curl -u -elastic -XPUT 'localhost:9200/twitter_friend_data_2_8/_mapping/input_type?pretty' -H 'Content-Type: application/json' -d'
 {
   "properties": {
