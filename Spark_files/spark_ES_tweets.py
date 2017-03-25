a62d8566a5a7c09432b523e5a6740ce1dd1ff141
Yan Jiang <enaj1125@gmail.com>
2017-03-25 15:39:59 -0700

    Update spark_ES_tweets.py

--- Spark_files/spark_ES_tweets.py
+++ Spark_files/spark_ES_tweets.py
@@ -10,6 +10,7 @@ from time import strftime
 from pyspark import SparkConf, SparkContext
 from elasticsearch import Elasticsearch
 
+
 # Elasticsearch setting
 ES_INDEX = 'twitter_data'
 ES_TYPE = 'inputs'
@@ -21,29 +22,41 @@ es = Elasticsearch(['ip-of-my-cluster'], http_auth=('elastic', 'changeme'), veri
 
 # Map function is used to clean/filter data, and write to ES
 def map_func(line):
+    
+    # connect to elasticsearch
     es = Elasticsearch(['ip-of-my-cluster'], http_auth=('elastic', 'changeme'), verify_certs=False)
+    
+    # load json data line by line
     each_line = json.loads(line)
+    
+    # Set three bolean variable to check data quality: completeness 
     user_exisit, time_exisit, text_exist = False, False, False
     retweet_id = []
 
+    # check if user id info is complete
     if 'user' in each_line:
         if 'id' in each_line['user']:
             usr_id = each_line['user']['id']
             user_exisit = True
 
+    # check if timestamp info is complete        
     if 'timestamp_ms' in each_line:
+        # original data formatted in UTC, need to be trancated and reformatted to %Y-%m-%d 
         raw_time = float(each_line['timestamp_ms'][:10])
         t_time = datetime.datetime.utcfromtimestamp(raw_time)
         t_time = t_time.strftime('%Y-%m-%d')
         time_exisit = True
 
+    # check if text info is complete    
     if 'text' in each_line:
         t_text = each_line['text']
         text_exisit = True
-
+        
+    # if data quality check pass, write data into Elasticsearch
     if user_exisit and time_exisit and text_exisit:
         doc = {'usr_id': usr_id, 'ttext': t_text, 'ttimes': t_time}
         es.index(index= ES_INDEX, doc_type='inputs', body=doc)
+        
         return [('key', doc)]
     else:
         return []
